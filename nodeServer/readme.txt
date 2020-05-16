9c7cd452eb07d6384de4afbbbe5dcbd6f7b83cc0
Gavin Wood <gavinneilwood@gmail.com>
2020-05-16 22:27:33 +0100

    New server pages.

--- nodeServer/readme.txt
+++ nodeServer/readme.txt
@@ -2,14 +2,17 @@ webserver1.js
 
 Just writes back "Hello World" to the client
 
+
 webserver2.js
 
 Sends a simple web page to the client
 which uses an HTML page to write "Hello World!"
 
+
 webserver3.js
 
 Might go back and put one of my earlier attempts up. Let me know
+
 
 webserver4.js
 
@@ -17,21 +20,14 @@ Uses socket.io to indicate when a mouse press on screen has happened
 
 Sends a mouse event when down and one when it's released
 
-TODO webserver5.js
 
-Respond to touch
+webserver5.js
 
-TODO webserver6.js
+Sends the client multiple web pages.
+This means we can put the different examples on different pages
 
-Respond to a concept of player
 
-TODO webserver7.js
 
-Create the concept of a game
-
-TODO webserver8.js
-
-Combine with graphics
 
 
 
---
+++ nodeServer/webserver5.js
@@ -0,0 +1,31 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', (req, res) => {
+  //console.log("Req: " + req);
+
+  res.sendFile(__dirname + '/webserver5_page1.html');
+});
+
+app.get('/webserver5_page2.html', (req, res) => {
+  //console.log("Req: " + req);
+
+  res.sendFile(__dirname + '/webserver5_page2.html');
+});
+
+io.on('connection', (socket) => {
+  
+  console.log("Server has a connection");
+   
+  // Send to the client
+  socket.emit('news', { news: 'hello new client' });
+  
+  // Receive an event from the client
+  socket.on('myevent', (data) => {
+    console.log("This data was received by the server from the client:");
+	console.log(data);
+  });
+});
