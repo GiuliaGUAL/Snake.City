949e1ac8e365d099a34b9cc310c7f1eb18bf7577
kihapper <playful.intervention@gmail.com>
2020-05-17 23:30:49 +0100

    bug fixes for state change

--- frontend/game.js
+++ frontend/game.js
@@ -45,55 +45,51 @@ function startup() {
 
 function touchStartA(event) {
     event.preventDefault();
-    buttonA.innerHTML = "buttonA - ON";
     A_pressed = true;
     buttonStateUpdate();
 }
 
 function touchStartB(event) {
     event.preventDefault();
-    buttonB.innerHTML = "buttonB - ON";
     B_pressed = true;
     buttonStateUpdate();
 }
 
 function touchEndA(event) {
     event.preventDefault();
-    buttonA.innerHTML = "buttonA - OFF";
     A_pressed = false;
     buttonStateUpdate();
 }
 
 function touchEndB(event) {
     event.preventDefault();
-    buttonB.innerHTML = "buttonB - OFF"
     B_pressed = false;
     buttonStateUpdate();
 }
 
 function buttonStateUpdate() {
     if (!A_pressed && !B_pressed) {
-        CURRENT_BUTTON = BUTTON.NONE;
+        currentBUTTON = BUTTON.NONE;
         buttonA.classList.remove("active");
         buttonB.classList.remove("active");
     }
     else if (A_pressed && B_pressed) {
-        CURRENT_BUTTON = BUTTON.BOTH;
+        currentBUTTON = BUTTON.BOTH;
         buttonA.classList.add("active");
         buttonB.classList.add("active");
         connected = true;
     }
     else if (A_pressed && !B_pressed) {
-        CURRENT_BUTTON = BUTTON.A;
+        currentBUTTON = BUTTON.A;
         buttonA.classList.add("active");
         buttonB.classList.remove("active");
     }
     else {
-        CURRENT_BUTTON = BUTTON.B;
+        currentBUTTON = BUTTON.B;
         buttonA.classList.remove("active");
         buttonB.classList.add("active");
     }
-    debugText.innerHTML = CURRENT_BUTTON;
+    debugText.innerHTML = "currentBUTTON: " + currentBUTTON;
     //Calls the state manager
     stateManage();
 }
@@ -101,22 +97,22 @@ function buttonStateUpdate() {
 function stateManage() {
     //Changes state based on if the two screens have connected or not.
     if (!connected) {
-        if (CURRENT_BUTTON === BUTTON.A || BUTTON.B) {
-            CURRENT_STATE = STATE.START;
+        if (currentBUTTON == BUTTON.A || currentBUTTON ==BUTTON.B) {
+            currentSTATE = STATE.START;
         }
         else {
-            CURRENT_STATE = STATE.INITIATE;
+            currentSTATE = STATE.INITIATE;
         }
     }
 
     if (connected) {
-        if (CURRENT_BUTTON === BUTTON.BOTH) {
-            CURRENT_STATE = STATE.CONNECTED;
+        if (currentBUTTON === BUTTON.BOTH) {
+            currentSTATE = STATE.CONNECTED;
         }
         else {
-            CURRENT_STATE = STATE.PAUSED;
+            currentSTATE = STATE.PAUSED;
         }
     }
-    stateText.innerHTML = CURRENT_STATE;
+    stateText.innerHTML = "currentSTATE: " + currentSTATE;
 }
 
